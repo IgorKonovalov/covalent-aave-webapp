d5137148e12709a268e93804d49e187c4c59ad2d
Igor Konovalov <Igor_Konovalov@epam.com>
2021-02-25 11:58:35 +0300

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.main}>
+      <header className={styles.header}>
+        <p>
+          Covalent-Aave webapp test
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
